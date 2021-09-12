34362967d92bcb2354fe49e31bbbe444839d26dc
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-12 19:58:16 +0200

    added db context to services

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BirthdayAPI.Persistence.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +29,7 @@ namespace BirthdayAPI
         {
             services.AddControllers();
             services.AddSwaggerGen();
-            //services.AddDbContext<RestDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Local")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
