81624f21534d40c2847b6295a5098e3c6a6a6f02
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-15 18:47:43 +0200

    updated account repository to implement interface

--- Persistence/Repositories/AccountRepository.cs
+++ Persistence/Repositories/AccountRepository.cs
@@ -8,11 +8,26 @@ using System.Threading.Tasks;
 
 namespace BirthdayAPI.Persistence.Repositories
 {
-    public class AccountRepository : BaseRepository<Account>
+    public class AccountRepository : BaseRepository<Account>, IAccountRepository
     {
         public AccountRepository(ApplicationDbContext context) : base(context)
         {
 
+        }
+
+        public async Task AddAccount(Account newAccount)
+        {
+            await base.Add(newAccount);
+        }
+
+        public void EditAccount(Account account)
+        {
+            base.Edit(account);
+        }
+
+        public async Task<Account> GetAccountById(int id)
+        {
+            return await base.GetById(id);
         }
 
         public async Task<IEnumerable<Account>> GetAccountsWithTag()
@@ -20,6 +35,16 @@ namespace BirthdayAPI.Persistence.Repositories
             // TODO: hladat podla nazvu / emailu, resp. zoradenie podla nazvu / datumu vytvorenia uctu...
             return await _context.Accounts
                 .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Account>> GetAllAccounts()
+        {
+            return await base.GetAll();
+        }
+
+        public void RemoveAccount(Account account)
+        {
+            base.Remove(account);
         }
     }
 }
