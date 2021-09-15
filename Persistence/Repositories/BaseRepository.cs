90e0f607f11fff6522fbb0bb49fe41432d8bb3ef
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-15 18:31:53 +0200

    Created an abstract base repository

---
+++ Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,34 @@
+﻿using BirthdayAPI.Persistence.Context;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BirthdayAPI.Persistence.Repositories
+{
+    public abstract class BaseRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public virtual async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        public virtual async Task<IEnumerable<T>> GetAll()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public abstract Task Add(T entity);
+
+        public abstract void Edit(T entity);
+        
+        public abstract void Remove(T entity);
+        
+
+    }
+}
