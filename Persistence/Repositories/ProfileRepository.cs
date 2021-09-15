17b8e70cf1577bebc650461f4de4ccf2e530e4bd
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-15 18:36:33 +0200

    created profile repository

---
+++ Persistence/Repositories/ProfileRepository.cs
@@ -0,0 +1,18 @@
+﻿using BirthdayAPI.Persistence.Context;
+using BirthdayAPI.Persistence.Models.Normal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Persistence.Repositories
+{
+    public class ProfileRepository : BaseRepository<Profile>
+    {
+        public ProfileRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+        
+    }
+}
