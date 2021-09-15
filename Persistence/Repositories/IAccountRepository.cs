a9e89469df4d65290fd78a9dd14a363bcbf33691
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-15 18:47:28 +0200

    created interface for account repository

---
+++ Persistence/Repositories/IAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using BirthdayAPI.Persistence.Models.Normal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Persistence.Repositories
+{
+    interface IAccountRepository
+    {
+        Task<IEnumerable<Account>> GetAllAccounts();
+        Task<Account> GetAccountById(int id);
+        Task AddAccount(Account newAccount);
+        void EditAccount(Account account);
+        void RemoveAccount(Account account);
+    }
+}
