e26cda71f525475cdb3ba08393c1cc7e1db715b7
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-15 19:32:22 +0200

    implemented unit of work for saving

---
+++ Persistence/Units/SaveUnit.cs
@@ -0,0 +1,24 @@
+﻿using BirthdayAPI.Interfaces;
+using BirthdayAPI.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Persistence.Units
+{
+    public class SaveUnit : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SaveUnit(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
