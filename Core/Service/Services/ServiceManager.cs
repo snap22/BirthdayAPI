f1bab3195d7dfd2d805c33e0ba2a76cd8730c3d8
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-16 17:17:46 +0200

    Added IProfileService

--- Core/Service/Services/ServiceManager.cs
+++ Core/Service/Services/ServiceManager.cs
@@ -11,13 +11,16 @@ namespace BirthdayAPI.Core.Service.Services
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IAccountService> _lazyAccountService;
+        private readonly Lazy<IProfileService> _lazyProfileService;
+
 
         public ServiceManager(IRepositoryManager repositoryManager, IMapper mapper)
         {
             _lazyAccountService = new Lazy<IAccountService>(() => new AccountService(repositoryManager, mapper));
+            _lazyProfileService = new Lazy<IProfileService>(() => new ProfileService(repositoryManager, mapper));
         }
         public IAccountService AccountService => _lazyAccountService.Value;
 
-        public IProfileService ProfileService => throw new NotImplementedException();
+        public IProfileService ProfileService => _lazyProfileService.Value;
     }
 }
