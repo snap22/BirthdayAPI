3175ca57f3b23f15f07882408608c5c6253e1e18
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-16 16:28:30 +0200

    added ProfileRepository

--- Core/Service/Repositories/RepositoryManager.cs
+++ Core/Service/Repositories/RepositoryManager.cs
@@ -9,15 +9,17 @@ namespace BirthdayAPI.Core.Service.Repositories
     public class RepositoryManager : IRepositoryManager
     {
         private readonly Lazy<IAccountRepository> _lazyAccountRepository;
+        private readonly Lazy<IProfileRepository> _lazyProfileRepository;
         private readonly Lazy<IUnitOfWork> _lazyUnitOfWork;
 
         public RepositoryManager(ApplicationDbContext context)
         {
             _lazyAccountRepository = new Lazy<IAccountRepository>(() => new AccountRepository(context));
+            _lazyProfileRepository = new Lazy<IProfileRepository>(() => new ProfileRepository(context));
             _lazyUnitOfWork = new Lazy<IUnitOfWork>(() => new SaveUnit(context));
         }
         public IAccountRepository AccountRepository => _lazyAccountRepository.Value;
-
+        public IProfileRepository ProfileRepository => _lazyProfileRepository.Value;
         public IUnitOfWork UnitOfWork => _lazyUnitOfWork.Value;
     }
 }
