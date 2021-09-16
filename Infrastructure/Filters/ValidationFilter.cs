9011eb6b0f2a1bf3c1ff88b8d68cdf19b1a26f7d
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-16 14:57:40 +0200

    Created filter for validation

---
+++ Infrastructure/Filters/ValidationFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Infrastructure.Filters
+{
+    public class ValidationFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (context.ModelState.IsValid == false)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+
+            await next();
+        }
+    }
+}
