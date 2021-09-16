92ee4c1cc6c5f7a3d732d934b36baea7cd20da88
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-16 18:06:21 +0200

    created interface for contacts

---
+++ Core/Service/Services/Abstractions/IContactService.cs
@@ -0,0 +1,18 @@
+﻿using BirthdayAPI.Core.Service.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Core.Service.Services.Abstractions
+{
+    public interface IContactService
+    {
+        Task<ContactDto> GetContact(int ContactId);
+        Task<IEnumerable<ContactDto>> GetContacts();
+        Task<IEnumerable<ContactDto>> GetSpecificContacts();
+        Task<ContactDto> CreateContact(ContactDto contact);
+        Task<ContactDto> UpdateContact(int contactId, ContactDto contact);
+        Task<ContactDto> RemoveContact(int contactId);
+    }
+}
