5031dc78dc496d5c1eb1e8f1633b66d435234a19
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-16 14:23:11 +0200

    Simplified methods

--- Infrastructure/Presentation/Controllers/AccountsController.cs
+++ Infrastructure/Presentation/Controllers/AccountsController.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using BirthdayAPI.Infrastructure.Persistence.Context;
-using BirthdayAPI.Core.Domain.Entities;
 using BirthdayAPI.Core.Service.Services.Abstractions;
 using BirthdayAPI.Core.Service.DTOs;
 
@@ -34,8 +30,7 @@ namespace BirthdayAPI.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<AccountDto>> GetAccount(int id)
         {
-            var foundAccount = await _service.AccountService.GetAccount(id);
-            return Ok(foundAccount);
+            return Ok(await _service.AccountService.GetAccount(id));
         }
 
         // PUT: api/Accounts/5
@@ -62,8 +57,7 @@ namespace BirthdayAPI.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult<AccountDto>> DeleteAccount(int id)
         {
-            var deletedAccount = await _service.AccountService.RemoveAccount(id);
-            return Ok(deletedAccount);
+            return Ok(await _service.AccountService.RemoveAccount(id));
         }
 
         
