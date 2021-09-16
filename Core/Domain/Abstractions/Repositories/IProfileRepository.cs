010133abd0219b19f56bf7a86d4604da2744fbf5
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-16 16:27:46 +0200

    Created interface for profile repository

---
+++ Core/Domain/Abstractions/Repositories/IProfileRepository.cs
@@ -0,0 +1,20 @@
+﻿using BirthdayAPI.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Core.Domain.Abstractions.Repositories
+{
+    public interface IProfileRepository
+    {
+        Task<IEnumerable<Profile>> GetAllProfiles();
+        Task<Profile> GetProfileById(int id);
+        Task<IEnumerable<Profile>> GetSpecificProfiles();
+        Task AddProfile(Profile newprofile);
+        void EditProfile(Profile profile);
+        void RemoveProfile(Profile profile);
+        bool ProfileWithIdExists(int profileId);
+        bool ProfileWithUsernameExists(string username);
+    }
+}
