fdd5234a7f0d106bc1aff94cc2fafe1578e66943
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-16 16:09:02 +0200

    changed dateCreated to string

--- Core/Service/DTOs/AccountDto.cs
+++ Core/Service/DTOs/AccountDto.cs
@@ -10,6 +10,6 @@ namespace BirthdayAPI.Core.Service.DTOs
         public int AccountId { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public DateTime DateCreated { get; set; }
+        public string DateCreated { get; set; }
     }
 }
