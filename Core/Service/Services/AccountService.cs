c2faaa2a0a6b33c9155422189e535977604b1d4f
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-16 14:47:40 +0200

    Added checking whether given account exists

--- Core/Service/Services/AccountService.cs
+++ Core/Service/Services/AccountService.cs
@@ -5,6 +5,7 @@ using BirthdayAPI.Core.Domain.Abstractions.Repositories;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using BirthdayAPI.Core.Service.Services.Abstractions;
+using BirthdayAPI.Core.Domain.Exceptions;
 
 namespace BirthdayAPI.Core.Service.Services
 {
@@ -29,12 +30,10 @@ namespace BirthdayAPI.Core.Service.Services
 
         public async Task<AccountDto> GetAccount(int accountId)
         {
-            if (!_repository.AccountRepository.AccountExists(accountId))
-            {
-                // Ucet Neexistuje
-            }
-                
+            ThrowErrorIfAccountDoesntExist(accountId);
+
             var foundAccount = await _repository.AccountRepository.GetAccountById(accountId);
+
             return _mapper.Map<AccountDto>(foundAccount);
         }
 
@@ -52,7 +51,10 @@ namespace BirthdayAPI.Core.Service.Services
 
         public async Task<AccountDto> RemoveAccount(int accountId)
         {
+            ThrowErrorIfAccountDoesntExist(accountId);
+
             var foundAccount = await _repository.AccountRepository.GetAccountById(accountId);
+
             _repository.AccountRepository.RemoveAccount(foundAccount);
             await _repository.UnitOfWork.CompleteAsync();
             return _mapper.Map<AccountDto>(foundAccount);
@@ -60,14 +62,21 @@ namespace BirthdayAPI.Core.Service.Services
 
         public async Task<AccountDto> UpdateAccount(int accountId, AccountDto account)
         {
-            var existingAccount = await _repository.AccountRepository.GetAccountById(accountId);
+            ThrowErrorIfAccountDoesntExist(accountId);
 
+            var existingAccount = await _repository.AccountRepository.GetAccountById(accountId);
             _mapper.Map(account, existingAccount);
             _repository.AccountRepository.EditAccount(existingAccount);
             await _repository.UnitOfWork.CompleteAsync();
 
             var foundAccount = await _repository.AccountRepository.GetAccountById(account.AccountId);
             return _mapper.Map<AccountDto>(foundAccount);
+        }
+
+        private void ThrowErrorIfAccountDoesntExist(int accountId)
+        {
+            if (_repository.AccountRepository.AccountExists(accountId) == false)
+                throw new NotFoundException($"Account with id: {accountId} doesn't exist!");
         }
     }
 }
