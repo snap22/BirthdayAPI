6048eacf311012172636313bece76744746f2b94
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-16 16:28:07 +0200

    updated with the implementation of the interface

--- Core/Service/Repositories/ProfileRepository.cs
+++ Core/Service/Repositories/ProfileRepository.cs
@@ -4,15 +4,55 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BirthdayAPI.Core.Domain.Abstractions.Repositories;
 
 namespace BirthdayAPI.Core.Service.Repositories
 {
-    public class ProfileRepository : BaseRepository<Profile>
+    public class ProfileRepository : BaseRepository<Profile>, IProfileRepository
     {
         public ProfileRepository(ApplicationDbContext context) : base(context)
         {
 
         }
-        
+
+        public async Task AddProfile(Profile newprofile)
+        {
+            await base.Add(newprofile);
+        }
+
+        public void EditProfile(Profile profile)
+        {
+            base.Edit(profile);
+        }
+
+        public async Task<IEnumerable<Profile>> GetAllProfiles()
+        {
+            return await base.GetAll();
+        }
+
+        public async Task<Profile> GetProfileById(int id)
+        {
+            return await base.GetById(id);
+        }
+
+        public Task<IEnumerable<Profile>> GetSpecificProfiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ProfileWithIdExists(int profileId)
+        {
+            return _context.Profiles.Any(p => p.ProfileId == profileId);
+        }
+
+        public bool ProfileWithUsernameExists(string username)
+        {
+            return _context.Profiles.Any(p => p.Username == username);
+        }
+
+        public void RemoveProfile(Profile profile)
+        {
+            base.Remove(profile);
+        }
     }
 }
