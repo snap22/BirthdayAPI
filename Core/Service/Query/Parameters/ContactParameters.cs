99481caba7d33eade667ea57260bcbad3df796b2
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-17 18:57:05 +0200

    Created parameters for contact

---
+++ Core/Service/Query/Parameters/ContactParameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Core.Service.Query.Parameters
+{
+    public class ContactParameters : QueryStringParameters
+    {
+        public ContactParameters()
+        {
+            OrderBy = "ContactId";
+        }
+        public string Name { get; set; }
+        public int MinMonth { get; set; }
+        public int MaxMonth { get; set; }
+        public bool IsValidMonthRange()
+        {
+            return MinMonth < MaxMonth;
+        }
+        public bool IsValidMonth(int month)
+        {
+            return month < 12 && month > 0;
+        }
+    }
+}
