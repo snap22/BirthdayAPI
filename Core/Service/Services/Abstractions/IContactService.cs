22382c9361a2783c51f2248d014bdedd887f7f94
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-17 19:08:58 +0200

    Added contact parameters to get method

--- Core/Service/Services/Abstractions/IContactService.cs
+++ Core/Service/Services/Abstractions/IContactService.cs
@@ -1,4 +1,5 @@
 ﻿using BirthdayAPI.Core.Service.DTOs;
+using BirthdayAPI.Core.Service.Query.Parameters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace BirthdayAPI.Core.Service.Services.Abstractions
     public interface IContactService
     {
         Task<ContactDto> GetContact(int ContactId);
-        Task<IEnumerable<ContactDto>> GetContacts();
+        Task<IEnumerable<ContactDto>> GetContacts(ContactParameters parameters);
         Task<ContactDto> CreateContact(ContactDto contact);
         Task<ContactDto> UpdateContact(int contactId, ContactDto contact);
         Task<ContactDto> RemoveContact(int contactId);
