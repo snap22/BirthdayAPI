bfac3810cd595bee781da22d9ec5a737714df3ac
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-17 14:49:44 +0200

    Created class for query parameters

---
+++ QueryParameters/QueryStringParameters.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace BirthdayAPI.QueryParameters
+{
+    public abstract class QueryStringParameters
+    {
+        const int MAX_PAGE_SIZE = 50;
+        private int _pageSize = 10;
+        public int Page { get; set; } = 1;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > MAX_PAGE_SIZE || value <= 0) ? MAX_PAGE_SIZE : value;
+            }
+        }
+    }
+}
