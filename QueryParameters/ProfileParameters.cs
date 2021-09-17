59f474031ac90e98aedbf42e2912cab588b46c3b
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-17 14:49:57 +0200

    Created children of query parameter class

---
+++ QueryParameters/ProfileParameters.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.QueryParameters
+{
+    public class ProfileParameters : QueryStringParameters
+    {
+    }
+}
