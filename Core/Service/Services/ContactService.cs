3d062f1d27ba6db997cd978d36f6f0f4481ca000
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-17 19:09:20 +0200

    created contact service

---
+++ Core/Service/Services/ContactService.cs
@@ -0,0 +1,38 @@
+﻿using BirthdayAPI.Core.Service.DTOs;
+using BirthdayAPI.Core.Service.Query.Parameters;
+using BirthdayAPI.Core.Service.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Core.Service.Services
+{
+    public class ContactService : IContactService
+    {
+        public Task<ContactDto> CreateContact(ContactDto contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ContactDto> GetContact(int ContactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ContactDto>> GetContacts(ContactParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ContactDto> RemoveContact(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ContactDto> UpdateContact(int contactId, ContactDto contact)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
