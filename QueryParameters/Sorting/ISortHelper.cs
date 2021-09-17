b0e19d1609f316a14ed90fabc0a882e42c4e9cb6
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-17 17:30:14 +0200

    Created interface for sorting

---
+++ QueryParameters/Sorting/ISortHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.QueryParameters.Sorting
+{
+    public interface ISortHelper<T> where T : class
+    {
+        IQueryable<T> ApplySort(IQueryable<T> entities, string orderByQueryString);
+    }
+}
