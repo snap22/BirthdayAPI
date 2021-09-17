7066fa601edd1db2b89c019f131b09c996cee1cb
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-17 16:38:19 +0200

    Added parameters for filtering

--- QueryParameters/AccountParameters.cs
+++ QueryParameters/AccountParameters.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 
 namespace BirthdayAPI.QueryParameters
 {
+
     public class AccountParameters : QueryStringParameters
     {
+        public uint MinYearOfCreation { get; set; }
+        public uint MaxYearOfCreation { get; set; } = (uint)DateTime.Now.Year;
+        public bool IsValidYearRange() => MinYearOfCreation < MaxYearOfCreation;
+        public string EmailAddress { get; set; }
     }
 }
