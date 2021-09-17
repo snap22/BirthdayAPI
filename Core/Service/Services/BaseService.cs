34a6e5330e4cf9bbe9bf9039fe244e9732d66274
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-17 19:13:49 +0200

    Created parent service for storing DI-ed interfaces

---
+++ Core/Service/Services/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BirthdayAPI.Core.Domain.Abstractions.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Core.Service.Services
+{
+    public class BaseService
+    {
+        protected readonly IRepositoryManager _repository;
+        protected readonly IMapper _mapper;
+        public BaseService(IRepositoryManager repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+    }
+}
