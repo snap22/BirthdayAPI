c9faf8479e2d6de8863779aeb72cd4ae1a133c3f
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-17 19:20:17 +0200

    Added profile id as a parameter for get methods

--- Core/Service/Services/Abstractions/IContactService.cs
+++ Core/Service/Services/Abstractions/IContactService.cs
@@ -9,8 +9,8 @@ namespace BirthdayAPI.Core.Service.Services.Abstractions
 {
     public interface IContactService
     {
-        Task<ContactDto> GetContact(int ContactId);
-        Task<IEnumerable<ContactDto>> GetContacts(ContactParameters parameters);
+        Task<ContactDto> GetContact(int profileId, int contactId);
+        Task<IEnumerable<ContactDto>> GetContacts(int profileId, ContactParameters parameters);
         Task<ContactDto> CreateContact(ContactDto contact);
         Task<ContactDto> UpdateContact(int contactId, ContactDto contact);
         Task<ContactDto> RemoveContact(int contactId);
