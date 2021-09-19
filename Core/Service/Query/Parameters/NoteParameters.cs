ba08528fa25ffdd04f7b3aad5ceaad571c896b71
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-19 13:59:08 +0200

    Creatd parameters for note entity

---
+++ Core/Service/Query/Parameters/NoteParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Core.Service.Query.Parameters
+{
+    public class NoteParameters : QueryStringParameters
+    {
+        public NoteParameters()
+        {
+            OrderBy = "NoteId";
+        }
+
+        public string Title { get; set; }
+    }
+}
