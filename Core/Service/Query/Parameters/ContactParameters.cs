11ab4fd7c5cd2fe856e86a49a07e61bd72508b8a
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-19 13:55:08 +0200

    Added default parameter set for months

--- Core/Service/Query/Parameters/ContactParameters.cs
+++ Core/Service/Query/Parameters/ContactParameters.cs
@@ -7,20 +7,43 @@ namespace BirthdayAPI.Core.Service.Query.Parameters
 {
     public class ContactParameters : QueryStringParameters
     {
+        private const int MIN_VALUE = 1;
+        private const int MAX_VALUE = 12;
+
+        private int _minMonth = 1;
+        private int _maxMonth = 12;
         public ContactParameters()
         {
             OrderBy = "ContactId";
         }
         public string Name { get; set; }
-        public int MinMonth { get; set; }
-        public int MaxMonth { get; set; }
+        public int MinMonth { 
+            get
+            {
+                return _minMonth;
+            }
+
+            set
+            {
+                _minMonth = (value < MIN_VALUE || value > MAX_VALUE) ? MIN_VALUE : value;
+            }
+        }
+        public int MaxMonth { 
+            get
+            {
+                return _maxMonth;
+            }
+
+            set
+            {
+                _maxMonth = (value < MIN_VALUE || value > MAX_VALUE) ? MAX_VALUE : value;
+            }
+        }
         public bool IsValidMonthRange()
         {
             return MinMonth < MaxMonth;
         }
-        public bool IsValidMonth(int month)
-        {
-            return month < 12 && month > 0;
-        }
+        
+        
     }
 }
