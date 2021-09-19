60ac201dba9b31ae6044f94c33c92ffe553e281c
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-19 16:07:12 +0200

    removed profileid parameter from getGiftsOfContact

--- Core/Domain/Abstractions/Repositories/IGiftRepository.cs
+++ Core/Domain/Abstractions/Repositories/IGiftRepository.cs
@@ -1,4 +1,5 @@
 ﻿using BirthdayAPI.Core.Domain.Entities;
+using BirthdayAPI.Core.Service.Query.Parameters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,8 @@ namespace BirthdayAPI.Core.Domain.Abstractions.Repositories
 {
     public interface IGiftRepository
     {
-        Task<IEnumerable<Gift>> GetGifts();
+        Task<IEnumerable<Gift>> GetGifts(GiftParameters parameters);
+        Task<IEnumerable<Gift>> GetGiftsOfContact(int contactId, GiftParameters parameters);
         Task<Gift> GetGiftById(int id);
         Task AddGift(Gift newGift);
         void EditGift(Gift gift);
