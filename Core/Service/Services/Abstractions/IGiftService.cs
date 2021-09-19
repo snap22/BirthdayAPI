4bb05e8a516bf2a5710b906b8ec93c4c8636edc4
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-19 16:20:33 +0200

    Created interface for gift service

--- Core/Service/Services/Abstractions/IGiftService.cs
+++ Core/Service/Services/Abstractions/IGiftService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BirthdayAPI.Core.Service.DTOs;
+using BirthdayAPI.Core.Service.Query.Parameters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,10 @@ namespace BirthdayAPI.Core.Service.Services.Abstractions
 {
     public interface IGiftService
     {
+        Task<GiftDto> GetGift(int profileId, int contactId,  int giftId);
+        Task<IEnumerable<GiftDto>> GetGifts(int profileId, int contactId, GiftParameters parameters);
+        Task<GiftDto> CreateGift(int profileId, int contactId, GiftDto Gift);
+        Task<GiftDto> UpdateGift(int profileId, int contactId, int giftId, GiftDto Gift);
+        Task<GiftDto> RemoveGift(int profileId, int contactId, int giftId);
     }
 }
