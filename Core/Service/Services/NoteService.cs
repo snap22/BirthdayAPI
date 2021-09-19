4ff0c1b4967ca862c1b6fd25f79a9fc4431f2a57
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-19 14:28:59 +0200

    Implemented methods

--- Core/Service/Services/NoteService.cs
+++ Core/Service/Services/NoteService.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using BirthdayAPI.Core.Domain.Abstractions.Repositories;
+using BirthdayAPI.Core.Domain.Entities;
+using BirthdayAPI.Core.Domain.Exceptions;
 using BirthdayAPI.Core.Service.DTOs;
 using BirthdayAPI.Core.Service.Query.Parameters;
 using BirthdayAPI.Core.Service.Services.Abstractions;
@@ -14,29 +16,83 @@ namespace BirthdayAPI.Core.Service.Services
     {
         public NoteService(IRepositoryManager repository, IMapper mapper) : base(repository, mapper) { }
 
-        public Task<NoteDto> CreateNote(int profileId, NoteDto Note)
+        public async Task<NoteDto> CreateNote(int profileId, NoteDto note)
         {
-            throw new NotImplementedException();
+            ThrowErrorIfProfileDoesntExist(profileId);
+            note.ProfileId = profileId;
+
+            var newNote = _mapper.Map<Note>(note);
+            await _repository.NoteRepository.AddNote(newNote);
+            await _repository.UnitOfWork.CompleteAsync();
+
+            return note;
         }
 
-        public Task<NoteDto> GetNote(int profileId, int noteId)
+        public async Task<NoteDto> GetNote(int profileId, int noteId)
         {
-            throw new NotImplementedException();
+            ThrowErrorIfProfileDoesntExist(profileId);
+            ThrowErrorIfNoteDoesntExist(noteId);
+
+            var foundNote = await _repository.NoteRepository.GetNoteById(noteId);
+            ThrowErrorIfProfilesNotTheSame(foundNote.ProfileId, profileId);
+            return _mapper.Map<NoteDto>(foundNote);
         }
 
-        public Task<IEnumerable<NoteDto>> GetNotes(int profileId, NoteParameters parameters)
+        public async Task<IEnumerable<NoteDto>> GetNotes(int profileId, NoteParameters parameters)
         {
-            throw new NotImplementedException();
+            ThrowErrorIfProfileDoesntExist(profileId);
+            var foundNotes = await _repository.NoteRepository.GetNotesOfProfile(profileId, parameters);
+            return _mapper.Map<IEnumerable<NoteDto>>(foundNotes);
         }
 
-        public Task<NoteDto> RemoveNote(int profileId, int noteId)
+        public async Task<NoteDto> RemoveNote(int profileId, int noteId)
         {
-            throw new NotImplementedException();
+            ThrowErrorIfProfileDoesntExist(profileId);
+            ThrowErrorIfNoteDoesntExist(noteId);
+
+            var foundNote = await _repository.NoteRepository.GetNoteById(noteId);
+            ThrowErrorIfProfilesNotTheSame(foundNote.ProfileId, profileId);
+
+            _repository.NoteRepository.RemoveNote(foundNote);
+            await _repository.UnitOfWork.CompleteAsync();
+            
+            return _mapper.Map<NoteDto>(foundNote);
         }
 
-        public Task<NoteDto> UpdateNote(int profileId, int noteId, NoteDto Note)
+        public async Task<NoteDto> UpdateNote(int profileId, int noteId, NoteDto Note)
         {
-            throw new NotImplementedException();
+            ThrowErrorIfProfileDoesntExist(profileId);
+            ThrowErrorIfNoteDoesntExist(noteId);
+
+            var foundNote = await _repository.NoteRepository.GetNoteById(noteId);
+            ThrowErrorIfProfilesNotTheSame(foundNote.ProfileId, profileId);
+
+            if (foundNote.ProfileId != Note.ProfileId)
+                throw new BadRequestException("Cannot change the profile of notes!");
+
+            _mapper.Map(Note, foundNote);
+            _repository.NoteRepository.EditNote(foundNote);
+            await _repository.UnitOfWork.CompleteAsync();
+
+            return _mapper.Map<NoteDto>(foundNote);
+        }
+
+        private void ThrowErrorIfProfileDoesntExist(int profileId)
+        {
+            if (_repository.ProfileRepository.ProfileWithIdExists(profileId) == false)
+                throw new NotFoundException($"Profile with id: {profileId} does not exist!");
+        }
+
+        private void ThrowErrorIfNoteDoesntExist(int noteId)
+        {
+            if (_repository.NoteRepository.NoteWithIdExists(noteId) == false)
+                throw new NotFoundException($"Note with id: {noteId} does not exist!");
+        }
+
+        private void ThrowErrorIfProfilesNotTheSame(int profileId1, int profileId2)
+        {
+            if (profileId1 != profileId2)
+                throw new BadRequestException("Note does not belong to this profile!");
         }
     }
 }
