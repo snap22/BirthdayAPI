be555b277445e72bdfcf8326c60a5f50beb04ca6
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-19 15:57:11 +0200

    Created parameter for gift

---
+++ Core/Service/Query/Parameters/GiftParameters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Core.Service.Query.Parameters
+{
+    public class GiftParameters : QueryStringParameters
+    {
+        private const double DEFAULT_PRICE = 0;
+        private double _minPrice;
+        private double _maxPrice;
+
+        public string Name { get; set; }
+        public double MinPrice
+        { 
+            get
+            {
+                return _minPrice;
+            }
+
+            set
+            {
+                _minPrice = (value < 0) ? DEFAULT_PRICE : value;
+            }
+        }
+
+        public double MaxPrice
+        {
+            get
+            {
+                return _maxPrice;
+            }
+
+            set
+            {
+                _maxPrice = (value < 0) ? MinPrice : value;
+            }
+        }
+
+        public bool IsValidPriceRange() => _minPrice <= _maxPrice;
+    }
+}
