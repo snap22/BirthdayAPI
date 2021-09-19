442cf93274763856e025d6c2def2b71e2ae8c637
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-19 14:10:29 +0200

    Created interface methods

---
+++ Core/Service/Services/Abstractions/INoteService.cs
@@ -0,0 +1,18 @@
+﻿using BirthdayAPI.Core.Service.DTOs;
+using BirthdayAPI.Core.Service.Query.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Core.Service.Services.Abstractions
+{
+    public interface INoteService
+    {
+        Task<NoteDto> GetNote(int profileId, int noteId);
+        Task<IEnumerable<NoteDto>> GetNotes(int profileId, NoteParameters parameters);
+        Task<NoteDto> CreateNote(int profileId, NoteDto Note);
+        Task<NoteDto> UpdateNote(int profileId, int noteId, NoteDto Note);
+        Task<NoteDto> RemoveNote(int profileId, int noteId);
+    }
+}
