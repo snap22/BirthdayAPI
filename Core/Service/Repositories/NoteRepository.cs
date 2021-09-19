e53a2cbca03ffcb99eff1754125b65ac210a31ef
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-19 14:10:06 +0200

    Added interface & base methods

---
+++ Core/Service/Repositories/NoteRepository.cs
@@ -0,0 +1,53 @@
+﻿using BirthdayAPI.Core.Domain.Abstractions.Repositories;
+using BirthdayAPI.Core.Domain.Entities;
+using BirthdayAPI.Core.Service.Query.Parameters;
+using BirthdayAPI.Core.Service.Query.Sorting;
+using BirthdayAPI.Infrastructure.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Core.Service.Repositories
+{
+    public class NoteRepository : BaseRepository<Note>, INoteRepository
+    {
+        public NoteRepository(ApplicationDbContext context, ISortHelper<Note> sortHelper) : base(context, sortHelper) { }
+
+        
+        public Task AddNote(Note newNote)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditNote(Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Note> GetNoteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Note>> GetNotes(NoteParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Note>> GetNotesOfProfile(int profileId, NoteParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NoteWithIdExists(int noteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveNote(Note note)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
