277ce493a9cee093a1881d6947c7300db789d3a4
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-20 12:52:01 +0200

    Created abstract parent controller class for storing injected dependencies

---
+++ Infrastructure/Presentation/Controllers/BasicController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BirthdayAPI.Core.Service.Services.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayAPI.Infrastructure.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class BasicController : ControllerBase
+    {
+        protected readonly IServiceManager _service;
+
+        public BasicController(IServiceManager service)
+        {
+            _service = service;
+        }
+    }
+}
