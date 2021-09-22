d1e85143caca77d3fc493cc85fc1dc330f2b67d9
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-22 21:24:12 +0200

    implemented generate links method

--- Infrastructure/LinkResources/LinkGenerators/NoteLinks.cs
+++ Infrastructure/LinkResources/LinkGenerators/NoteLinks.cs
@@ -1,4 +1,5 @@
 ﻿using BirthdayAPI.Core.Service.DTOs;
+using BirthdayAPI.Infrastructure.Presentation.Controllers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using System;
@@ -14,7 +15,36 @@ namespace BirthdayAPI.Infrastructure.LinkResources.LinkGenerators
 
         public override LinkedEntity<NoteDto> GenerateLinksForOneEntity(HttpContext httpContext, NoteDto entity)
         {
-            throw new NotImplementedException();
+            var linkedNote = new LinkedEntity<NoteDto> { Value = entity };
+            linkedNote.Links = new List<Link>
+            {
+                new Link(
+                    href: _linkGenerator.GetUriByAction(httpContext, nameof(NotesController.GetNote), "Notes", values : new {profileId = entity.ProfileId, noteId = entity.NoteId}),
+                    rel: "self",
+                    method: "GET"
+                    ),
+                new Link(
+                    href: _linkGenerator.GetUriByAction(httpContext, nameof(NotesController.PutNote), "Notes", values : new {profileId = entity.ProfileId, noteId = entity.NoteId}),
+                    rel: "update_note",
+                    method: "PUT"
+                    ),
+                new Link(
+                    href: _linkGenerator.GetUriByAction(httpContext, nameof(NotesController.DeleteNote), "Notes", values : new {profileId = entity.ProfileId, noteId = entity.NoteId}),
+                    rel: "delete_note",
+                    method: "DELETE"
+                    ),
+                new Link(
+                    href: _linkGenerator.GetUriByAction(httpContext, nameof(ProfilesController.GetProfile), "Profiles", values : new {id = entity.ProfileId}),
+                    rel: "self_profile",
+                    method: "GET"
+                    ),
+                new Link(
+                    href: _linkGenerator.GetUriByAction(httpContext, nameof(NotesController.GetNotes), "Notes", values : new {profileId = entity.ProfileId}),
+                    rel: "other_notes",
+                    method: "GET"
+                    ),
+            };
+            return linkedNote;
         }
     }
 }
