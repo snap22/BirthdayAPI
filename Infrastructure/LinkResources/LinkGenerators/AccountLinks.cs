4281339a3f35bb59371146ae71afa655ed1d5347
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-22 19:57:44 +0200

    Created class for generating links for account

---
+++ Infrastructure/LinkResources/LinkGenerators/AccountLinks.cs
@@ -0,0 +1,34 @@
+﻿using BirthdayAPI.Core.Service.DTOs;
+using BirthdayAPI.Infrastructure.Presentation.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Infrastructure.LinkResources.LinkGenerators
+{
+    public class AccountLinks : BasicLinks<AccountDto>
+    {
+        public AccountLinks(LinkGenerator linkGenerator) : base(linkGenerator) { }
+
+        public override LinkedEntity<AccountDto> GenerateLinksForOneEntity(HttpContext httpContext, AccountDto entity)
+        {
+            var linkedAccount = new LinkedEntity<AccountDto> { Value = entity };
+
+            linkedAccount.Links = new List<Link>
+            {
+                new Link(
+                    _linkGenerator.GetUriByAction(httpContext, nameof(AccountsController.GetAccount), "Accounts", values: new { id = entity.AccountId }), "self", "GET"),
+                new Link(
+                    _linkGenerator.GetUriByAction(httpContext, action : nameof(AccountsController.PutAccount), controller: "Accounts", values: new {id = entity.AccountId }), "update_account", "PUT"),
+                new Link(
+                    _linkGenerator.GetUriByAction(httpContext, nameof(AccountsController.DeleteAccount), "Accounts", values: new { id = entity.AccountId }), "delete_account", "DELETE"),
+                new Link(
+                    _linkGenerator.GetUriByAction(httpContext, nameof(ProfilesController.GetProfileByAccount), "Profiles", values: new { accountId = entity.AccountId }), "self_profile", "GET")
+            };
+            return linkedAccount;
+        }
+    }
+}
