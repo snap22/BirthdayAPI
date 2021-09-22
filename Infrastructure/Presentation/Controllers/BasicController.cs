cd7b18e44a3a61508e6c7c281ecfa5a5be4d69d9
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-22 18:19:31 +0200

    Injected links creator to constructor parameter

--- Infrastructure/Presentation/Controllers/BasicController.cs
+++ Infrastructure/Presentation/Controllers/BasicController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BirthdayAPI.Core.Service.Services.Abstractions;
+using BirthdayAPI.Infrastructure.LinkResources;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 
 namespace BirthdayAPI.Infrastructure.Presentation.Controllers
 {
@@ -14,9 +16,14 @@ namespace BirthdayAPI.Infrastructure.Presentation.Controllers
     {
         protected readonly IServiceManager _service;
 
-        public BasicController(IServiceManager service)
+        // TODO: zmenit
+        protected LinksCreator _linksCreator;
+        protected LinkGenerator _linksGenerator;
+
+        public BasicController(IServiceManager service, LinksCreator linksCreator)
         {
             _service = service;
+            _linksCreator = linksCreator;
         }
     }
 }
