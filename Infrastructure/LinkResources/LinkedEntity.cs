21d654ff2bdbc8dce6136e299de5ed496dd281bd
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-22 18:18:44 +0200

    Created a wrapped entity with links

---
+++ Infrastructure/LinkResources/LinkedEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Infrastructure.LinkResources
+{
+    public class LinkedEntity<T> where T : class
+    {
+        public T Value { get; set; }
+        public List<Link> Links { get; set; } = new List<Link>();
+    }
+}
