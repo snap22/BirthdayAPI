54e0fbd2c7d3f9940837ccac5213287f025770fe
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-22 20:26:45 +0200

    Implemented generate links method

--- Infrastructure/LinkResources/LinkGenerators/ContactLinks.cs
+++ Infrastructure/LinkResources/LinkGenerators/ContactLinks.cs
@@ -1,4 +1,5 @@
 ﻿using BirthdayAPI.Core.Service.DTOs;
+using BirthdayAPI.Infrastructure.Presentation.Controllers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using System;
@@ -14,7 +15,43 @@ namespace BirthdayAPI.Infrastructure.LinkResources.LinkGenerators
 
         public override LinkedEntity<ContactDto> GenerateLinksForOneEntity(HttpContext httpContext, ContactDto entity)
         {
-            throw new NotImplementedException();
+            var linkedContact = new LinkedEntity<ContactDto> { Value = entity };
+
+            linkedContact.Links = new List<Link>
+            {
+                new Link(
+                    href: _linkGenerator.GetUriByAction(httpContext, nameof(ContactsController.GetContact), "Contacts", values : new {profileId = entity.ProfileId, contactId = entity.ContactId}),
+                    rel: "self",
+                    method: "GET"
+                    ),
+                new Link(
+                    href: _linkGenerator.GetUriByAction(httpContext, nameof(ContactsController.PutContact), "Contacts", values : new {profileId = entity.ProfileId, contactId = entity.ContactId}),
+                    rel: "update_contact",
+                    method: "PUT"
+                    ),
+                new Link(
+                    href: _linkGenerator.GetUriByAction(httpContext, nameof(ContactsController.DeleteContact), "Contacts", values : new {profileId = entity.ProfileId, contactId = entity.ContactId}),
+                    rel: "delete_contact",
+                    method: "DELETE"
+                    ),
+                new Link(
+                    href: _linkGenerator.GetUriByAction(httpContext, nameof(GiftsController.GetGifts), "Gifts", values : new {profileId = entity.ProfileId, contactId = entity.ContactId}),
+                    rel: "self_gifts",
+                    method: "GET"
+                    ),
+                new Link(
+                    href: _linkGenerator.GetUriByAction(httpContext, nameof(ProfilesController.GetProfile), "Profiles", values : new {id = entity.ProfileId}),
+                    rel: "self_profile",
+                    method: "GET"
+                    ),
+                new Link(
+                    href: _linkGenerator.GetUriByAction(httpContext, nameof(ContactsController.GetContacts), "Contacts", values : new {profileId = entity.ProfileId}),
+                    rel: "other_contacts",
+                    method: "GET"
+                    ),
+            };
+
+            return linkedContact;
         }
     }
 }
