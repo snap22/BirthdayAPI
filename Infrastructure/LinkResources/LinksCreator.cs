4029234ed87265cad8a37d3c7c58f2b097be23b4
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-22 20:01:37 +0200

    Added generators as props

--- Infrastructure/LinkResources/LinksCreator.cs
+++ Infrastructure/LinkResources/LinksCreator.cs
@@ -1,4 +1,5 @@
 ﻿using BirthdayAPI.Core.Service.DTOs;
+using BirthdayAPI.Infrastructure.LinkResources.LinkGenerators;
 using BirthdayAPI.Infrastructure.Presentation.Controllers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,41 +14,18 @@ namespace BirthdayAPI.Infrastructure.LinkResources
 {
     public class LinksCreator
     {
-        private readonly LinkGenerator _linkGenerator;
+        public AccountLinks Account { get; private set; }
+        public ProfileLinks Profile { get; private set; }
 
         public LinksCreator(LinkGenerator linkGenerator)
         {
-            _linkGenerator = linkGenerator;
+            Account = new AccountLinks(linkGenerator);
+            Profile = new ProfileLinks(linkGenerator);
         }
-        public LinkedEntity<AccountDto> GenerateLinksForAccount(HttpContext httpContext, AccountDto account)
-        {
-            var linkedAccount = new LinkedEntity<AccountDto> { Value = account };
+        
 
-            
-            var links = new List<Link>
-            {
-                new Link(
-                    _linkGenerator.GetUriByAction(httpContext, nameof(AccountsController.GetAccount), "Accounts", values: new { id = account.AccountId }), "self", "GET"),
-                new Link(
-                    _linkGenerator.GetUriByAction(httpContext, action : nameof(AccountsController.PutAccount), controller: "Accounts", values: new {id = account.AccountId }), "update_account", "PUT"),
-                new Link(
-                    _linkGenerator.GetUriByAction(httpContext, nameof(AccountsController.DeleteAccount), "Accounts", values: new { id = account.AccountId }), "delete_account", "DELETE"),
-                new Link(
-                    _linkGenerator.GetUriByAction(httpContext, nameof(ProfilesController.GetProfileByAccount), "Profiles", values: new { accountId = account.AccountId }), "self_profile", "GET")
-            };
+        
 
-            linkedAccount.Links = links;
-            return linkedAccount;
-        }
 
-        public IEnumerable<LinkedEntity<AccountDto>> GenerateLinksForAccounts(HttpContext httpContext, IEnumerable<AccountDto> accounts)
-        {
-            var linkedAccounts = new List<LinkedEntity<AccountDto>>();
-            foreach (var account in accounts)
-            {
-                linkedAccounts.Add(GenerateLinksForAccount(httpContext, account));
-            }
-            return linkedAccounts;
-        }
     }
 }
