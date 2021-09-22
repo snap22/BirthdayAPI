1d272694880bbcc69ef0c87d589a3c4c2a710171
snap22 <39875321+snap22@users.noreply.github.com>
2021-09-22 19:49:21 +0200

    Created base class for link generators

---
+++ Infrastructure/LinkResources/LinkGenerators/BasicLinks.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayAPI.Infrastructure.LinkResources.LinkGenerators
+{
+    public abstract class BasicLinks<T> where T : class 
+    {
+        public abstract LinkedEntity<T> GenerateLinksForOneEntity(HttpContext httpContext, T entity);
+        public virtual IEnumerable<LinkedEntity<T>> GenerateLinksForManyEntities(HttpContext httpContext, IEnumerable<T> entities)
+        {
+            var linkedEntities = new List<LinkedEntity<T>>();
+
+            foreach (var entity in entities)
+            {
+                linkedEntities.Add(GenerateLinksForOneEntity(httpContext, entity));
+            }
+
+            return linkedEntities;
+        }
+    }
+}
